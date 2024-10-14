7fe158594bd5c3d75fc580d49375b063cb9c893a
giorgiadifranco <giorgiadf08@gmail.com>
2024-10-14 14:55:14 +0200

    fix card

--- assets/script.js
+++ assets/script.js
@@ -53,13 +53,14 @@ for (let i = 0 ; i < teamMembers.length; i++){
     //console.log(member);
 
     let markup =  `<div class="col-1 my-2">
-                    <div class="card ">
-                    <img  class="" src="${img}" alt="">
+                    <div class="card d-flex ">
+                    
                     </div>
                   </div>
-                  <div class="col-3 ">
+                  <div class="col-2 ">
+                  
                     <div class="card bg-dark p-2">
-                        
+                        <img  class="float-start w-5" src="${img}" alt="">
                         <h3 class="text-white ">${name}</h3>
                         <p class="text-white">${role}</p>
                         <p class="text-primary">${email}</p>
